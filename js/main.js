fdbab87819d590eb6f1f15b94be212bebf27a073
Ben Shrimpton <ben@blackandblackcreative.com>
2014-05-05 19:41:52 -0400

    Added age gate, added mobile and desktop sliders, still needs lots of CSS and mobile debugging

--- js/main.js
+++ js/main.js
@@ -1,10 +1,53 @@
+//we need to know wether we;'re mobile or not.
+// Lets use scren width for this.
+var screenWidth = $(window).width();
+if ( screenWidth <= 640) {
+	$('body').addClass('mobile');
+}
+else {
+	$('body').removeClass('mobile');
+}
+var isMobile = $('body').hasClass('mobile');
+//alert(isMobile);
+
+// Do Fitvids.
+$('.video-wrapper').fitVids();
+
+
+
+$('.menu a[href*=#]:not([href=#])').click(function(e) {
+
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+
+      $('html,body').stop().animate({
+
+        scrollTop: target.offset().top
+
+      }, 300, 'easeInOutCubic', function(){
+
+        $('.menu').removeClass('open');
+
+      });
+      return false;
+    }
+  }
+});
 
 
 
 
 
 
-  
+// Main toggle menu fucntion.
+$('.menu-toggle-button-wrapper, .menu-closer').click(function() {
+
+    $('.menu').toggleClass('open');
+
+});
+
 
 
 /*
@@ -24,113 +67,135 @@ $('#contact-row').waitForImages(function() {
   }, $.noop, true);
 */
   
-
-function resizeFirstSection(){
+ //only do this is not mobile. 
+if ( !isMobile ) {
+	function resizeFirstSection(){
 	var winheight = $(window).height();
 	var winWidth = $(window).width();
-	var firstSection = $('.section');
-
-	firstSection.css({
-		'width': winWidth+'px',
-		'height': winheight+'px'
-	});
+	var firstSection = $('.inner');
+	
+	  firstSection.css({
+	    'min-width': winWidth+'px',
+	    'height': winheight+'px'
+	  });
+	}
+	resizeFirstSection();
 }
+
+
   
 
 
 
 
- // $('nav a').click(function (e) {
 
- // 		//alert("fufuf");
- // 		var newUrl = $(this).data('url');
- // 		var newTitle = $(this).data('title');
- // 		var target = $(this.hash);
- // 		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
- //        //overwrite current entry in history to store the scroll position:
- //        stateData = {
- //            path: window.location.href,
- //            scrollTop: $(window).scrollTop()
- //        };
- //        window.history.replaceState(stateData, newTitle, newUrl);
-
-         
-      
- //      if (target.length) {
-        
- //        $('html,body').animate({ scrollTop: target.offset().top }, 300, function(){
- //        	setTimeout(closer, 100);
- //        });
-
- //    }
-    
- //    function closer(){
- //    	$('.main-nav').animate({
- //    		'left':'-280px'
- //    	}, 300)
- //    }
-
-
-       
-
- //        //load new page:
- //        stateData = {
- //            path: window.location.href,
- //            scrollTop: 0
- //        };
- //        window.history.pushState(stateData, newTitle, newUrl);
- //        e.preventDefault();
-
- //    });
-
-
-
-
-$(window).on('resize', function(){
-
-	resizeFirstSection();
-
-});
 
 
 $(document).ready(function(){
 
 	resizeFirstSection();
 
-  //USe the wait for image plugin.
-$.waitForImages.hasImgProperties = ['backgroundImage']
-$('#intro').waitForImages(function() {
-    $(this).addClass('loaded');
-}, $.noop, true);
+	//USe the wait for image plugin.
+	$.waitForImages.hasImgProperties = ['backgroundImage']
+	$('#intro').waitForImages(function() {
+	$(this).addClass('loaded');
+	}, $.noop, true);
 
 
-var $menu = $('nav#menu'), $html = $('html, body');
-
-  $menu.mmenu();
-  $menu.find( 'li > a' ).on(
-    'click',
-    function()
-    {
-      var href = $(this).attr( 'href' );
-
-      //  if the clicked link is linked to an anchor, scroll the page to that anchor 
-      if ( href.slice( 0, 1 ) == '#' )
-      {
-        $menu.one(
-          'closed.mm',
-          function()
-          {
-            setTimeout(
-              function()
-              {
-                $html.animate({
-                  scrollTop: $( href ).offset().top
-                } , 500 , 'easeInOutCubic');  
-              }, 10);  
-          });            
-      }
+	$(".royalSlider").royalSlider({
+        // options go here
+        // as an example, enable keyboard arrows nav
+        keyboardNavEnabled: true,
+        navigateByClick: false
     });
+    
+    
+
+	
+	
+
+ 
 
 
 }); //end dom ready
+
+
+
+
+$(window).on('resize', function(){
+
+resizeFirstSection();
+var screenWidth = $(window).width();
+var isMobile = $('body').hasClass('mobile');
+
+if ( screenWidth <= 640) {
+	$('body').addClass('mobile');
+}
+else {
+	$('body').removeClass('mobile');
+}
+
+
+
+}); 
+
+
+/* ########################### AGE GATE ########################### */
+function isChecked() {
+	if (!this.form.checkbox.checked) {
+		alert('You must agree to the terms and conditions to continue.');
+		return false;
+	}
+}
+
+$('#age-gate-form').validate({
+	rules: {
+		"Month_form": "required",
+		"Day_form": "required",
+		"Year_form": "required",
+		"checkbox": "required"
+	},
+	submitHandler: function(form) {
+			// do other things for a valid form
+			ageGateValidation();
+		}
+});
+
+function ageGateValidation () {
+	var a = $('#Country_form').val(); // put what ever age you want to be verified, and it will work!
+	var d = $('#Day_form').val(); // d = day
+	var m = $('#Month_form').val(); // m = month
+	var y = $('#Year_form').val(); //y = year
+	var t = new Date(); // t = “today"
+	var td = t.getDate(); // td = “today's date"
+	var tm = t.getMonth()+1; // tm = “this month" / remember January starts at 0, like arrays!
+	var ty = t.getFullYear()-a; //ty = “this year" / remember to subtract 18 from this year
+
+	if (y > ty) {
+		alert('Give it a couple years');
+		return false;
+	}
+	else {
+		alert('You can drink');
+		$.cookie('the_cookie', 'the_value', { expires: 7 });
+		$('.age-gate-background').addClass('gate-hidden');
+		//$('body').addClass('cookie-set');
+	}
+	return true;
+}
+
+//alert(the_cookie);
+
+if($.cookie('the_cookie') != null) {
+	//cookie exists - hide modal
+	$('.age-gate-background').addClass('gate-hidden');
+	//$('body').addClass('cookie-set');
+	//alert('cookie set');
+	//var ageGateDiv = $('.age-gate-background');
+	//console.log(ageGateDiv);
+}
+else {
+	//alert('cookie not set');
+}
+		
+		
